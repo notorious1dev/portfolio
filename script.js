592d0de4d99d7c9f43d48deeb95fa22027619a82
defektonec <sarjovskiy.dima@gmail.com>
2025-04-28 14:23:43 +0200

    browser memory theme allocation

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
 const themeToggle = document.getElementById("themeToggle");
-const body = document.getElementById("bodyTheme");
-themeToggle.addEventListener("click", ()=>
-{
-    if (body.classList.contains("dark-theme"))
-    {
-        body.classList.remove("dark-theme");
-        body.classList.add("light-theme");
+const savedTheme = localStorage.getItem("theme");
+
+if (savedTheme) {
+    applyTheme(savedTheme);
+} else {
+    applyTheme("light");
+}
+
+themeToggle.addEventListener("click", () => {
+    const currentTheme = document.body.classList.contains('dark-theme') ? 'dark' : 'light';
+    const newTheme = currentTheme === 'dark' ? 'light' : 'dark';
+    applyTheme(newTheme);
+    localStorage.setItem('theme', newTheme);
+});
+
+function applyTheme(theme) {
+    if (theme === "light") {
+        document.body.classList.remove("dark-theme");
+        document.body.classList.add("light-theme");
+    } else {
+        document.body.classList.remove("light-theme");
+        document.body.classList.add("dark-theme");
     }
-    else
-    {
-        body.classList.remove("light-theme");
-        body.classList.add("dark-theme");
-    }
-})
+}
